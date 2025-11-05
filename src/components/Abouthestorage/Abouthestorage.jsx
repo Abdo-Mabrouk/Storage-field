3be0ce593930a7bf434d52f091c1c258e023c313
Abdelrhman Mabrouk <abdomabroukdev@gmail.com>
2025-11-05 13:34:41 +0200

    edit tixt

--- src/components/Abouthestorage/Abouthestorage.jsx
+++ src/components/Abouthestorage/Abouthestorage.jsx
@@ -5,9 +5,9 @@ export default function Abouthestorage() {
     <>
         <div className="col-span-12 lg:col-span-3 space-y-15">
             <Title text={"عن ميدان التخزين"}/>
-            <ol className=" list-disc px-2 lg:p-5 space-y-10 *:text-[24px]">
-                <li className=" lg:w-full">ميدان التخزين مقرها في السعودية بمدينة الرياض، مرخص من هيئة الغذاء والدواء، تقوم بتقديم خدمات التخزين والدعم اللوجستي</li>
-                <li className=" lg:w-full">و تقديم افضل المستودعات بمواصفات عالية ملائمة لجميع الشركات و الجهات الحكومية</li>
+            <ol className=" list-disc px-2 lg:p-5 space-y-10 *:lg:text-[24px]">
+                <li className="text-[20px] lg:w-full">ميدان التخزين مقرها في السعودية بمدينة الرياض، مرخص من هيئة الغذاء والدواء، تقوم بتقديم خدمات التخزين والدعم اللوجستي</li>
+                <li className="text-[20px] lg:w-full">و تقديم افضل المستودعات بمواصفات عالية ملائمة لجميع الشركات و الجهات الحكومية</li>
             </ol>
         </div>
     </>
