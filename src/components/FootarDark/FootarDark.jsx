7748174cb1968b89ea7646805e19ba77f1bb174e
Abdelrhman Mabrouk <abdomabroukdev@gmail.com>
2025-11-05 12:30:29 +0200

    -----------

--- src/components/FootarDark/FootarDark.jsx
+++ src/components/FootarDark/FootarDark.jsx
@@ -15,24 +15,24 @@ function FootarDark() {
         <>
             <div className="bg-black" >
                 <footer className=" FootarDark py-[64px]"  >
-                <div className="container  grid grid-cols-6 gap-[131px] items-center">
+                <div className="container mx-auto  grid grid-cols-6 justify-between gap-4 lg:gap-0 items-center">
                     <div className="lg:col-span-2 md:col-span-6 col-span-6 space-y-5">
-                        <div className="flex flex-col p-5 justify-center items-start gap-10">
-                        <Image src={logo} className=" w-[300px] lg:w-full h-auto"/>
+                        <div className="flex flex-col justify-center items-start gap-10">
+                        <Image src={logo} className=" max-w-[300px] lg:w-full h-auto"/>
                         <div className="text-white space-y-10">
-                            <p className=" text-[16px] font-[500">ميدان التخزين مقرها في السعودية بمدينة الرياض، مرخص من هيئة الغذاء والدواء، تقوم بتقديم خدمات التخزين والدعم اللوجستي</p>
-                            <ul className="flex justify-start items-center gap-[18px] ">
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faSnapchat} className="  text-lg md:text-[24px]" /></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faFacebook} className=" text-lg md:text-[24px]"/></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faLinkedinIn} className="   text-lg md:text-[24px]"/></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faXTwitter} className=" text-lg md:text-[24px]"/></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faYoutube} className="   text-lg md:text-[24px]"/></a></li>
+                            <p className=" text-[16px] font-[500]">ميدان التخزين مقرها في السعودية بمدينة الرياض، مرخص من هيئة الغذاء والدواء، تقوم بتقديم خدمات التخزين والدعم اللوجستي</p>
+                            <ul className="flex justify-start items-center gap-2 md:gap-[18px] ">
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faSnapchat} className="  text-lg md:text-[24px]" /></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faFacebook} className=" text-lg md:text-[24px]"/></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faLinkedinIn} className="   text-lg md:text-[24px]"/></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faXTwitter} className=" text-lg md:text-[24px]"/></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-white border-2 border-white rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faYoutube} className="   text-lg md:text-[24px]"/></a></li>
                             </ul>
                         </div>
                         </div>
                     </div>
                     <div className="lg:col-span-2 md:col-span-3 col-span-6 space-y-5 lg:px-20">
-                        <p className="text-white text-[24px] mb-[48px] font-[500] ">روابط مختصره</p>
+                        <p className="text-white text-center lg:text-start text-[24px] mb-[48px] font-[500] ">روابط مختصره</p>
                         <ul className="flex flex-col gap-5 *:cursor-pointer *:hover:text-orange-500 *:text-xl *:font-semibold *:text-white">
                             <li className="relative before:absolute before:w-0 before:-bottom-3 hover:before:w-10 before:transition-all before:duration-300 before:start-5 before:h-1 before:bg-orange-500 " ><Link className={ pathname === "/" ? "text-[20px] font-semibold text-orange-500" :"cursor-pointer hover:text-orange-500 text-[20px] font-semibold text-white"} href="/"> الرئيسيه </Link></li>
                             <li className="relative before:absolute before:w-0 before:-bottom-3 hover:before:w-10 before:transition-all before:duration-300 before:start-5 before:h-1 before:bg-orange-500 " ><Link className={ pathname === "/WhoWeAre" ? "text-[20px] font-semibold text-orange-500" :"cursor-pointer hover:text-orange-500 text-[20px] font-semibold text-white"} href="/WhoWeAre"> من نحن </Link></li>
@@ -41,7 +41,7 @@ function FootarDark() {
                         </ul>
                     </div>
                     <div className="lg:col-span-2 md:col-span-3 col-span-6 space-y-5 text-2xl md:text-start text-white">
-                        <p className=" text-[24px] mb-[48px] font-[500]  text-start ">طرق التواصل</p>
+                        <p className=" text-[24px] mb-[48px] font-[500]  text-center lg:text-start ">طرق التواصل</p>
                         <ul className=" space-y-10">
                             <li> <a className="text-white flex items-center text-lg md:text-xl " ><span className="mx-3 size-[40px] flex items-center justify-center border-2 border-gray-300 rounded-full"><FontAwesomeIcon icon={faPhoneVolume} className="text-white text-[16px]" /></span>966 55448669+ -  9223331156 </a></li>
                             <li> <a className="text-white flex items-center text-lg md:text-xl " ><span className="mx-3 size-[40px] flex items-center justify-center border-2 border-gray-300 rounded-full"><FontAwesomeIcon icon={faEnvelope} className="text-white text-[16px]" /></span>info@GGHHGG.com</a> </li>
--- src/components/footar/Footar.jsx
+++ src/components/footar/Footar.jsx
@@ -15,24 +15,24 @@ function Footar() {
         <>
             <div className="bg-black/50" >
                 <footer className=" footer py-[64px]"  >
-                <div className="container  grid grid-cols-6 gap-[131px] items-center">
+                <div className="container mx-auto grid grid-cols-6 justify-between gap-4 lg:gap-0 items-center">
                     <div className="lg:col-span-2 md:col-span-6 col-span-6 space-y-5">
-                        <div className="flex flex-col p-3 justify-center items-start gap-10">
-                        <Image src={logo} className=" w-[300px] md:w-full h-auto"/>
+                        <div className="flex flex-col justify-center items-start gap-10">
+                        <Image src={logo} className=" maxw-[300px] md:w-full h-auto"/>
                         <div className="text-black space-y-10">
                             <p className=" text-[16px] font-[500]">ميدان التخزين مقرها في السعودية بمدينة الرياض، مرخص من هيئة الغذاء والدواء، تقوم بتقديم خدمات التخزين والدعم اللوجستي</p>
-                            <ul className="flex justify-start items-center gap-[18px] ">
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faSnapchat} className="  text-lg md:text-[24px]" /></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faFacebook} className=" text-lg md:text-[24px]"/></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faLinkedinIn} className="   text-lg md:text-[24px]"/></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faXTwitter} className=" text-lg md:text-[24px]"/></a></li>
-                                <li><a href="#" className=" size-[56px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faYoutube} className="   text-lg md:text-[24px]"/></a></li>
+                            <ul className="flex justify-start items-center gap-2 md:gap-[18px] ">
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faSnapchat} className="  text-lg md:text-[24px]" /></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faFacebook} className=" text-lg md:text-[24px]"/></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faLinkedinIn} className="   text-lg md:text-[24px]"/></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faXTwitter} className=" text-lg md:text-[24px]"/></a></li>
+                                <li><a href="#" className=" size-[50px] md:size-[56px] text-black border-2 border-btext-black rounded-full flex justify-center items-center hover:bg-orange-500 hover:border-orange-500 hover:transition-colors duration-400"><FontAwesomeIcon icon={faYoutube} className="   text-lg md:text-[24px]"/></a></li>
                             </ul>
                         </div>
                         </div>
                     </div>
                     <div className="lg:col-span-2 md:col-span-3 col-span-6 space-y-5 lg:px-20">
-                        <p className="text-black text-[24px] mb-[48px] font-[500] ">روابط مختصره</p>
+                        <p className="text-black text-center lg:text-star text-[24px] mb-[48px] font-[500] ">روابط مختصره</p>
                         <ul className="flex flex-col gap-5 *:cursor-pointer *:hover:text-orange-500 *:text-xl *:font-semibold ">
                             <li className="relative before:absolute before:w-0 before:-bottom-3 hover:before:w-10 before:transition-all before:duration-300 before:start-5 before:h-1 before:bg-orange-500 " ><Link className={ pathname === "/" ? "text-[20px] font-semibold text-orange-500" :"cursor-pointer hover:text-orange-500 text-[20px] font-semibold text-[#666]"} href="/"> الرئيسيه </Link></li>
                             <li className="relative before:absolute before:w-0 before:-bottom-3 hover:before:w-10 before:transition-all before:duration-300 before:start-5 before:h-1 before:bg-orange-500 " ><Link className={ pathname === "/WhoWeAre" ? "text-[20px] font-semibold text-orange-500" :"cursor-pointer hover:text-orange-500 text-[20px] font-semibold text-[#666]"} href="/WhoWeAre"> من نحن </Link></li>
@@ -41,7 +41,7 @@ function Footar() {
                         </ul>
                     </div>
                     <div className="lg:col-span-2 md:col-span-3 col-span-6 space-y-5 text-2xl md:text-start text-black">
-                        <p className=" text-[24px] mb-[48px] font-[500]  text-start ">طرق التواصل</p>
+                        <p className=" text-[24px] mb-[48px] font-[500]  text-center lg:text-star">طرق التواصل</p>
                         <ul className=" space-y-10">
                             <li> <a className="text-black flex items-center text-lg md:text-xl " ><span className="mx-3 size-[40px] flex items-center justify-center border-2 border-gray-300 rounded-full"><FontAwesomeIcon icon={faPhoneVolume} className="text-black text-[16px]" /></span>966 55448669+ -  9223331156 </a></li>
                             <li> <a className="text-black flex items-center text-lg md:text-xl " ><span className="mx-3 size-[40px] flex items-center justify-center border-2 border-gray-300 rounded-full"><FontAwesomeIcon icon={faEnvelope} className="text-black text-[16px]" /></span>info@GGHHGG.com</a> </li>
